a21be1bbf0e7053ad4dbf6c4c72860acbc677b76
cngondo <devngondo@gmail.com>
2016-10-05 19:52:46 +0300

    made splashscreen appear only once

--- app/src/main/java/com/cerebral/motivationmonday/SplashActivity.java
+++ app/src/main/java/com/cerebral/motivationmonday/SplashActivity.java
@@ -1,8 +1,12 @@
 package com.cerebral.motivationmonday;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
@@ -11,7 +15,20 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        SharedPreferences pref = getSharedPreferences("ActivityPref", Context.MODE_PRIVATE);
+        if(pref.getBoolean("activity_executed", false)){
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            finish();
+        }else{
+            //Request for full screen window
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            setContentView(R.layout.activity_splash);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putBoolean("activity_executed", true);
+            editor.commit();
+        }
     }
 
     @Override
