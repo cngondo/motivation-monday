31d917b7fc4599fb3a91a8df8fe4a436024ca040
cngondo <devngondo@gmail.com>
2016-10-11 17:12:26 +0300

    login page design

--- app/src/main/java/com/cerebral/motivationmonday/LoginActivity.java
+++ app/src/main/java/com/cerebral/motivationmonday/LoginActivity.java
@@ -1,13 +1,23 @@
 package com.cerebral.motivationmonday;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
